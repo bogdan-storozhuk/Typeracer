42b4814f71084f5c2f57f76791c4fd7f5b9a88f8
Artem Manukian <artem.m.manukyan@gmail.com>
2019-06-25 23:39:25 +0300

    initial version

---
+++ server.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+server.listen(3000);
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', socket => {
+
+    socket.on('submitMessage', payload => {
+        socket.broadcast.emit('newMessage', payload);
+        socket.emit('newMessage', payload);
+    });
+
+});
